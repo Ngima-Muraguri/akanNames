2aabfa029fde4dd89033ca5a091bf3ac340f7ffe
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-05 22:46:00 +0300

    added html input

---
+++ script.js
@@ -0,0 +1,3 @@
+const femalenames={'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'}
+const malenames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+const daysof week=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
