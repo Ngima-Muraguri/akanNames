783e002af0af7c98e6c33794941c0748f113c811
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-06 17:01:43 +0300

    added form validation

--- script.js
+++ script.js
@@ -5,15 +5,24 @@ const malenames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 const daysof week=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 
+function calculateDate(){
+var dd= parseInt(document.getElementById('day').value;)
+var mm= parseInt(document.getElementById('month').value;)
+var yy= parseInt(document.getElementById('year').value;)
+var cc=(yy-1)/100+1;
 
-var day=document.getElementById('day').value;
-var month=document.getElementById('month').value;
-var year=document.getElementById('year').value;
-
+var form=document.getElementById('form')
 var female=document.getElementById('fgender').value;
 var male=document.getElementById('mgender').value;
 
 
 
+if(dd<=0 || dd>31){
+    alert("please put in a valid day")
+} else if(mm<= || mm>12){
+    alert("please put in a valid month")
+}
 
 
+
+};
