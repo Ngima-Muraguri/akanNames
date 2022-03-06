c0654dacf8ac44ea6a6a0777a86fa48d32c1160e
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-06 14:01:30 +0300

    added js variables

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const femalenames={'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'}
 const malenames=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 const daysof week=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+
+var day=document.getElementById('day').value;
+var month=document.getElementById('month').value;
+var year=document.getElementById('year').value;
+var female=document.getElementById('fgender').value;
+var male=document.getElementById('mgender').value;
+
+
+
+
+
