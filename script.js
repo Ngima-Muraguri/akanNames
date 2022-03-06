0267a7ed57eab549d6eda308fe3ddb5ce2df8c43
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-03-06 19:31:24 +0300

    added alert messages

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-let femaleName=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+var femaleName=['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
 
-let maleName=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+var maleName=['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
 
-let daysOfWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var daysOfWeek=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 
 function calculateDate(){
 var dd= parseInt(document.getElementById('day').value);
 var mm= parseInt(document.getElementById('month').value);
 var yy= parseInt(document.getElementById('year').value);
-var female=document.getElementById('gender').value;
-var male=document.getElementById('gender').value;
+
 
 var cc=(yy-1)/100+1;
 
@@ -27,37 +26,36 @@ if(dd<=0 || dd>31){
 } else if(mm<=0 || mm>12){
     alert("please put in a valid month")
 } else if (Math.abs(output) == 0 && gender === "male") {
-    alert("Your Name is " + maleName[0]);
+    alert("Hello there handsome! You were born on Sunday . You are " + maleName[0]);
 }else if (Math.abs(output) == 1 && gender === "male") {
-    alert("Your Name is " + maleName[1]);
+    alert("Hello there handsome! You were born on Monday . You are " + maleName[1]);
 }else if (Math.abs(output) == 2 && gender === "male") {
-    alert("Your Name is " + maleName[2]);
+    alert("Hello there handsome! You were born on Tuesday . You are " + maleName[2]);
 }else if (Math.abs(output) == 3 && gender === "male") {
-    alert("Your Name is " + maleName[3]);
+    alert("Hello there handsome! You were born on Wednesday . You are" + maleName[3]);
 }else if (Math.abs(output) == 4 && gender === "male") {
-    alert("Your Name is " + maleName[4]);
+    alert("Hello there handsome! You were born on Thursday . You are " + maleName[4]);
 }else if (Math.abs(output) == 5&& gender === "male") {
-    alert("Your Name is " + maleName[5]);
+    alert("Hello there handsome! You were born on Friday . You are " + maleName[5]);
 }else if (Math.abs(output) == 6 && gender === "male") {
-    alert("Your Name is " + maleName[6]);
+    alert("Hello there handsome! You were born on Saturday . You are " + maleName[6]);
 }else if (Math.abs(output) == 0 && gender === "female") {
-    alert("Your Name is " + femaleName[0]);
+    alert("Hello there Beautiful! You were born on Sunday . You are " + femaleName[0]);
 }else if (Math.abs(output) == 1 && gender === "female") {
-    alert("Your Name is " + femaleName[1]);
+    alert("Hello there Beautiful! You were born on Monday . You are " + femaleName[1]);
 }else if (Math.abs(output) == 2 && gender === "female") {
-    alert("Your Name is " + femaleName[2]);
+    alert("Hello there Beautiful! You were born on Tuesday . You are " + femaleName[2]);
 }else if (Math.abs(output) == 3 && gender === "female") {
-    alert("Your Name is " + femaleName[3]);
+    alert("Hello there Beautiful! You were born on Wednesday . You are " + femaleName[3]);
 }else if (Math.abs(output) == 4 && gender === "female") {
-    alert("Your Name is " + femaleName[4]);
+    alert("Hello there Beautiful! You were born on Thursday . You are " + femaleName[4]);
 }else if (Math.abs(output) == 5 && gender === "female") {
-    alert("Your Name is " + femaleName[5]);
-}else if (Math.abs(bornDay) == 6 && gender === "female") {
-    alert("Your Name is " + femaleName[6]);
+    alert("Hello there Beautiful! You were born on Friday . You are " + femaleName[5]);
+}else if (Math.abs(output) == 6 && gender === "female") {
+    alert("Hello there Beautiful! You were born on Saturday . You are " + femaleName[6]);
 }else {
     alert("inputs required")
 }
 
 
 };
-var dayOfBirth= parseInt(cc/4-2*cc-1+(5*yy))
